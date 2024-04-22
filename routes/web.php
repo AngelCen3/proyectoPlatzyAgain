b09254ccdf9b173aaf036fd586ed2c83173b10ed
BetoRobert <wyjawy@hotmail.com>
2024-04-22 01:00:54 -0500

    Vamos aprender Markdown

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {    //Viene por defecto
     return view('welcome');
+});*/
+
+Route::get('/', function () {
+    return 'Ruta home';
 });
+
+Route::get('/blog  ', function () {
+    return 'Listado de publicaciones';
+});
+
+Route::get('/blog/{slug}', function($slug) {
+    //simularemos una consulta a base de datos
+    return $slug;
+});
+Route::get('buscar', function(Request $request) {
+    //simularemos una consulta a base de datos
+    return $request->all();
+});
+
